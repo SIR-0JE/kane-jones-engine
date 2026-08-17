53e20435f68ecbfe2cce35bb7e1c0e39a7181545
SIR-OJE <your-email@example.com>
2026-08-17 13:51:57 +0100

    fix(router): route API traffic directly to Next.js App Router handler

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
-      },
-    ];
+  async rewrites() {
+    return process.env.NODE_ENV === "development"
+      ? [
+          {
+            source: "/api/:path*",
+            destination: "http://127.0.0.1:8000/api/:path*",
+          },
+        ]
+      : [];
   },
 };
 
